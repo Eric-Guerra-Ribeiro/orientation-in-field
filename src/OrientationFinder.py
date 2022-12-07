fa6d398f95df69a41c898fe84550b20e3c2d8dcb
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-12-07 15:57:16 -0300

    implement constructor

--- src/OrientationFinder.py
+++ src/OrientationFinder.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+
 import cv2
+
+Reference = namedtuple('Reference', ['img', 'angle', 'points', 'descriptor'])
 
 class OrientationFinder:
     """
@@ -9,5 +13,18 @@ class OrientationFinder:
     was taken.
     """
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ref_imgs, ref_angles) -> None:
+        """
+        Initializes the Orientation Findes
+        :param ref_imgs: List with the reference images
+        :param ref_angles: List with the angles of each reference image, in the same order as the images
+        """
+        self.detector = cv2.ORB_create(nfeatures=4500, scaleFactor=1.19)
+        self.matcher = cv2.FlannBasedMatcher(
+            indexParams={ 'algorithm':6, 'table_number':6, 'key_size':12, 'multi_probe_level':1},
+            searchParams={'checks': 50}
+        )
+        self.references = [
+            Reference(ref_img, ref_angles[i], *self.detector.detectAndCompute(ref_img, None))
+            for i, ref_img in enumerate(ref_imgs)
+        ]
