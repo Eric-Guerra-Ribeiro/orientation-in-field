b762d28c0f6d12b247311d489f69d4027c26bf42
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-12-07 13:50:18 -0300

    creating source files

---
+++ src/OrientationFinder.py
@@ -0,0 +1,13 @@
+import cv2
+
+class OrientationFinder:
+    """
+    Finds the orientation/side in the field based on the background
+
+    Has references images with known orientation and compares them
+    to a new image to estimate the orientation in which the image
+    was taken.
+    """
+    
+    def __init__(self) -> None:
+        pass
